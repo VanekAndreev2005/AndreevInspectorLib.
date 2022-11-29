bf8c3bf97636704ab5c896c688e40b90bb0290b7
VanekAndreev2005 <anda18276@gmail.com>
2022-11-29 16:18:44 +0900

    Initial commit

---
+++ InspectorLib/FunctionInsp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        public string Title { get; set; }
+        public string Inspect { get; set; }
+        public string Fio { get; set; }
+        public string Gosnomer { get; set; }
+
+        public FunctionInsp(string title, string inspect, string fio, string gosnomer)
+        {
+            this.Title = title;
+            this.Inspect = inspect;
+            this.Fio = fio;
+            this.Gosnomer = gosnomer;
+        }
+    }
+}
